a7ec66ffc058985e78283a07ab221e6119f7f11f
Sachitha Thulshan <110708675+SachithaThulshan@users.noreply.github.com>
2023-11-19 10:24:59 +0530

    Ex_Insert.java

--- Arrays/src/Ex_Insert.java
+++ Arrays/src/Ex_Insert.java
@@ -2,29 +2,18 @@ public class Ex_Insert {
     public static void main(String[] args) {
         int[] array = {12, 34, 56, 78, 43, 92};
         int[] nArray = new int[7];
-        int size = array.length;
-        int index = 2;
-        int add = 234;
-        //array[index+1] = add;
-        for (int i= array.length; i >= 0; i--){
+        int ind = 2;
 
-            if (i == 2)
-                nArray[2] = add;
-            else
-                nArray[i]=array[i-1];
-            System.out.println(nArray[i]);
-
+        for (int i=0; i< array.length; i++) {
+            nArray[i] = array[i];
         }
-        //array[2]=add;
-
-//        for (int i=0; i < size-1;  i++){
-//            System.out.println(array[i]);
-//            if(i == 2)
-//                nArray[i] = add;
-//            else
-//                nArray[i] = array[index +1];
-//            System.out.print(nArray[i] + " ");
-//        }
-
+        for (int i = array.length-1; i >= ind; i--) {
+            nArray[i+1]= nArray[i];
+            if (i == 2)
+                nArray[2] = 234;
+        }
+        for (int i : nArray) {
+            System.out.println(i);
+        }
     }
 }
