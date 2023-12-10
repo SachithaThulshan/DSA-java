c29114c3e42b0346ed1b1eee658d3c247486347b
Sachitha Thulshan <110708675+SachithaThulshan@users.noreply.github.com>
2023-12-11 00:05:28 +0530

    Algorithms Code

---
+++ Algorithms/src/Sorting.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Sorting {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(8);
+        numbers.add(3);
+        numbers.add(5);
+        numbers.add(1);
+        numbers.add(3);
+
+    }
+
+
+}
