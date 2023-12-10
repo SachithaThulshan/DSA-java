f59fca680509df8ea3cd3df6e12ac18f0c1608e1
Sachitha Thulshan <110708675+SachithaThulshan@users.noreply.github.com>
2023-12-11 00:07:24 +0530

    Book.java

--- Sets/src/Book.java
+++ Sets/src/Book.java
@@ -15,7 +15,12 @@ public class Book {
 class setHashLinked{
     public static void main(String[] args) {
         LinkedHashSet<Book> lhs = new LinkedHashSet<>();
-        Book b1 = new Book(123, "java","java a", "abc",10);
+        Book b1 = new Book(123, "java for bigginers","java a", "abc",10);
+        Book b2 = new Book(123, "C for bigginers","java a", "xyz",45);
+        Book b3 = new Book(123, "how to steel tempered","Nokalai owstroski", "qwe",110);
+        lhs.add(b1);
+        lhs.add(b2);
+        lhs.add(b3);
         for (Book b: lhs ) {
             System.out.println();
 
